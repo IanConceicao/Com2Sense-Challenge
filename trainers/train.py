Masking fixes, training fixes. Now uses softmax

trainers/train.py
@@ -222,11 +222,7 @@ def train(args, train_dataset, model, tokenizer):
 
             if args.training_phase == "pretrain":
                 # TODO: Mask the input tokens.
-                inputs["attention_mask"] = (torch.ones_like(
-                     inputs["attention_mask"])
-                     .to(args.device)
-                     .float()
-                     .masked_fill(inputs["attention_mask"] == 0, -1e9))
+                inputs["input_ids"], inputs["labels"] = mask_tokens(inputs["input_ids"], tokenizer, args)
 
             # TODO: See the HuggingFace transformers doc to properly get
             # the loss from the model outputs.
@@ -411,7 +407,9 @@ def evaluate(args, model, tokenizer, prefix="", data_split="test"):
                 logits = model(inputs["input_ids"], attention_mask=inputs["attention_mask"], labels=inputs["labels"])
                 eval_loss = logits[0].mean()
                 logits = logits[1]
-            #logits[1] = torch.nn.functional.softmax(logits[1])
+            
+            if args.training_phase != "pretrain":
+                logits = torch.nn.functional.softmax(logits, dim = -1)
 
             # End of TODO.
             ##################################################

trainers/train_utils.py
@@ -40,7 +40,7 @@ def mask_tokens(inputs, tokenizer, args, special_tokens_mask=None):
             tokenizer.get_special_tokens_mask(val, already_has_special_tokens=True)
                 for val in labels.tolist()
         ]
-        special_tokens_mask = torch.tensor(special_tokens_mask, dtype=torch.bool)
+        special_tokens_mask = torch.tensor(special_tokens_mask, dtype=torch.bool, device=args.device)
     else:
         special_tokens_mask = special_tokens_mask.bool()
 
@@ -51,7 +51,7 @@ def mask_tokens(inputs, tokenizer, args, special_tokens_mask=None):
     distribution.masked_fill_(special_tokens_mask, 0)
 
     #Convert distribution to bernoulli, says which indices to mask
-    bern = torch.bernoulli(distribution).type(torch.BoolTensor)
+    bern = torch.bernoulli(distribution).type(torch.BoolTensor).to(args.device)
 
     #Fill in all NON-Masked spots
     labels.masked_fill_(~bern, args.mlm_ignore_index)