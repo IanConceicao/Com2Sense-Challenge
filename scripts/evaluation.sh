c05d3fa579e46dc32808cf138d537af7155daf3a
IanConceicao <iancon234@gmail.com>
2022-03-16 06:38:22 +0000

    New evaluation. 45K steps

--- scripts/evaluation.sh
+++ scripts/evaluation.sh
@@ -7,35 +7,32 @@ python3 -m trainers.train \
   --model_name_or_path ${MODEL_TYPE} \
   --do_train \
   --do_eval \
-  --logging_steps 20 \
+  --logging_steps 50 \
   --per_gpu_eval_batch_size 4 \
-  --num_train_epochs 100.0 \
   --max_seq_length 128 \
   --output_dir "${TASK_NAME}/ckpts" \
   --task_name "${TASK_NAME}" \
   --data_dir "${DATA_DIR}" \
   --overwrite_output_dir \
-  --save_steps 10000 \
+  --save_steps 15000 \
   --warmup_steps 100 \
   --eval_split "dev" \
   --score_average_method "micro" \
-  --iters_to_eval 10000 20000 30000 \
+  --iters_to_eval 10000 30000 45000\
   --overwrite_output_dir \
   --eval_split "dev" \
-  --best_model_warmup_percent 0.75 \
-  --best_model_steps 40 \
+  --best_model_warmup_percent 0.70 \
+  --best_model_steps 50 \
 
-  #TODO: Put the correct params  before all of thesecomments:
-  #--per_gpu_train_batch_size <8 or 16> \
-  #--weight_decay <0.1 or 0.01> \
-  #--learning_rate  <0.00001 or 0.000001> \
+  #TODO: Put the correct params  before all of these comments:
+  # --per_gpu_train_batch_size <8 or 16> \
+  # --weight_decay <0.1 or 0.01> \
+  # --learning_rate  <0.00001 or 0.000001> \
+  # --num_train_epochs <225 if batch size is 8, 450 if batch size is 16>
 
-  #TODO: Immediately after running make sure Total optimization steps = 30000
-  #      After running save the com2sense/ckpts/ make sure there is checkpoint-10000 checkpoint-20000  checkpoint-30000 and checkpoint-best
-  #      Save the tensorboard run in /runs/<time-ran...> (should be the bottom-most one)
-  
-  #Ignore:
-  # --evaluate_during_training \
-  # --max_eval_steps 1000 \
-  # --do_not_load_optimizer \
+  # Immediately after running make sure Total optimization steps = 45000
+ 
+  # After it finishes save the com2sense/ckpts/ make sure there is checkpoint-15000 checkpoint-30000  checkpoint-45000 and checkpoint-best
+  #    All should have an eval_results_split_dev.txt in there with their performance
+  # Save the tensorboard run in /runs/<time-ran...> (should be the bottom-most one)
 
